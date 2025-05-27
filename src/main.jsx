57031a8969bdd3c236a1bcaf4edbea8b373ff2d6
Anirudh <anirudh.tvvb@gmail.com>
2025-05-28 00:04:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ConstructionHomepage />} />
+        <Route path="/" element={<AboutUsPage />} />
         <Route path="home" element={<ConstructionHomepage />} />
         <Route path="/about" element={<AboutUsPage />} />
       </Routes>
